c3999c3ec64750f81ea451cfdbacab9570a6da32
Mthyyy <mathgames@hotmail.ca>
2022-01-29 11:40:36 -0500

    m5 reste pas teste

--- pb_logique_seq/pb_logique_seq.gen/sources_1/bd/design_1/hdl/design_1_wrapper.vhd
+++ pb_logique_seq/pb_logique_seq.gen/sources_1/bd/design_1/hdl/design_1_wrapper.vhd
@@ -1,7 +1,7 @@
 --Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
 --Tool Version: Vivado v.2020.2 (win64) Build 3064766 Wed Nov 18 09:12:45 MST 2020
---Date        : Thu Jan 27 15:25:10 2022
+--Date        : Sat Jan 29 10:37:42 2022
 --Host        : DESKTOP-00KO0KK running 64-bit major release  (build 9200)
 --Command     : generate_target design_1_wrapper.bd
 --Design      : design_1_wrapper
--- pb_logique_seq/pb_logique_seq.srcs/sources_1/imports/new/calcul_param_1.vhd
+++ pb_logique_seq/pb_logique_seq.srcs/sources_1/imports/new/calcul_param_1.vhd
@@ -62,12 +62,48 @@ architecture Behavioral of calcul_param_1 is
          );
     signal fsm_EtatCourant, fsm_prochainEtat : etat_MEF;
     signal en_cpt: std_logic;
-    signal res_cpt: std_logic;   
+    signal res_cpt: std_logic;
+    signal val_cpt: std_logic_vector(15 downto 0);
+    signal frquence: std_logic_vector(15 downto 0);
+    signal division: std_logic_vector(15 downto 0);
+    signal max: std_logic_vector(15 downto 0);
+    
+component compteur_nbits is
+generic (nbits : integer := 16);
+   port ( clk             : in    std_logic; 
+          i_en            : in    std_logic; 
+          reset           : in    std_logic; 
+          o_val_cpt       : out   std_logic_vector (nbits-1 downto 0)
+          );
+end component;    
 
 ---------------------------------------------------------------------------------------------
 --    Description comportementale
 ---------------------------------------------------------------------------------------------
 begin 
+    
+    max <= "1011101110000000";
+    o_param <= division(15) & division(14)& division(13) & division(12) & division(11) & division(10) & division(9) & division(8);
+    
+    inst_cpt: compteur_nbits
+        port map(
+                clk => i_bclk,
+                i_en => en_cpt,
+                reset => res_cpt,
+                o_val_cpt => val_cpt
+                );
+                
+    calculFrequence: Process(fsm_EtatCourant)
+    begin
+        case fsm_EtatCourant is
+            when sta_6 => frquence <= val_cpt;
+            when others => frquence <= frquence;
+        end case;
+        
+     division <= std_logic_vector(to_signed(to_integer(signed(frquence)/ signed(max)),16));   
+     
+    end process;
+
     transition: Process(i_ech)
     begin
         case fsm_EtatCourant is
@@ -147,7 +183,7 @@ begin
             
             when sta_cpt =>
             en_cpt <= '1';
-            res_cpt <= '0';
+            res_cpt <= '1';
             
             when sta_4 =>
             en_cpt <= '1';
@@ -159,7 +195,7 @@ begin
             
             when sta_6 =>
             en_cpt <= '1';
-            res_cpt <= '1';
+            res_cpt <= '0';
             
             when others =>
             en_cpt <= '0';
